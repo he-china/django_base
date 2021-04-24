045ebb1bb7fa892d06dfae30ba95d1c4df4d1149
he <786912450@qq.com>
2021-04-24 20:58:45 +0800

    admin站点

--- bookmanager/book/models.py
+++ bookmanager/book/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+"""
+    1 创建的模型类需要继承自models.Model
+    2 系统会为我们自动创建id字段
+    3 字段：
+        字段名=models.类型(选项)
+"""
+
+
+class BookInfo(models.Model):
+    # id
+    # varchar(10)
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+
+# 准备人物列表信息的模型类
+class PeopleInfo(models.Model):
+    name = models.CharField(max_length=10)
+    gender = models.BooleanField()
+    # 外键约束：人物属于哪本书
+    book = models.ForeignKey(BookInfo, on_delete=models.CASCADE)
--- bookmanager/bookmanager/settings.py
+++ bookmanager/bookmanager/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 't47-e&l8lj@v_d_ejj3_sgocwan25z+amu7**3i&u&e=t$a0=)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# 默认是127.0.0.1
+ALLOWED_HOSTS = ['192.168.245.132', '127.0.0.1']
 
 
 # Application definition
@@ -104,9 +105,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
+# 'en-us'
+LANGUAGE_CODE = 'zh-Hans'
+# 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
