fcb1269eb83d42c07e3295e4cc917bb4d92209da
he <786912450@qq.com>
2021-04-24 21:09:16 +0800

    视图函数

--- bookmanager/book/views.py
+++ bookmanager/book/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+"""
+    视图就是一个python函数
+"""
+
+from django.http import HttpRequest
+from django.http import HttpResponse
+
+# 期望用户输入index来访问视图函数
+
+
+def index(request):
+
+    return HttpResponse('OK')
+
+
